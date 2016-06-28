84e7a906fab054cafcc11a2e5f39d7bde020538e
funzin <famistap@gmail.com>
2016-06-28 17:57:32 +0900

    Add example of logevent

--- first-firebase/ViewController.swift
+++ first-firebase/ViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         //FIRAnalytics.logEventWithName("featuredevent", parameters: nil)
+        // プロパティセット処理例 これを呼んだだけではユーザリストに入らない
+        FIRAnalytics.setUserPropertyString("unregistered", forName: "register_status")
+        
+        // イベント送信処理例 ↑を呼んだ後にこれを呼ぶと、このイベントに対して、{"register_status": "unregistered"}のプロパティが付く
+        FIRAnalytics.logEventWithName("sccontent", parameters: [
+            "name": "tap",
+            "full_text": "button"
+            ])
         // Do any additional setup after loading the view, typically from a nib.
     }
 
